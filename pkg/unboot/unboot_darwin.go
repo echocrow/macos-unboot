96e839e1b4bbda39118ba9eb75b7372559c70627
Crow <5488190+echocrow@users.noreply.github.com>
2021-12-08 22:53:26 +0100

    Add basic shutdown & restart

--- pkg/unboot/unboot_darwin.go
+++ pkg/unboot/unboot_darwin.go
@@ -1,11 +1,35 @@
 package unboot
 
+import (
+	"fmt"
+	"os/exec"
+)
+
+const (
+	shutdownEvent = "aevtrsdn"
+	restartEvent  = "aevtrrst"
+)
+
+func unboot(loginwindowEvent string) error {
+	osascript := fmt.Sprintf(
+		"tell app \"loginwindow\" to «event %s»",
+		loginwindowEvent,
+	)
+
+	err := exec.Command("osascript", "-e", osascript).Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // Shutdown shuts down macOS.
 func Shutdown() error {
-	return nil
+	return unboot(shutdownEvent)
 }
 
 // Restart restarts macOS.
 func Restart() error {
-	return nil
+	return unboot(restartEvent)
 }
